1ae1b728e72850d9da13d52fbf3cc1ec1c387403
Ayilay <georgetroulis@gmail.com>
2020-02-05 11:31:05 -0800

    Added Route and RouteStorage class

---
+++ app/src/main/java/com/example/walkwalkrevolution/Route.java
@@ -0,0 +1,17 @@
+package com.example.walkwalkrevolution;
+
+public class Route {
+    private String name;
+    private String date;
+
+    public Route(String n, String d) {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+}
---
+++ app/src/main/java/com/example/walkwalkrevolution/RouteStorage.java
@@ -0,0 +1,18 @@
+package com.example.walkwalkrevolution;
+
+import java.util.List;
+
+public class RouteStorage {
+    private List<Route> routes;
+
+    public RouteStorage() {
+
+    }
+
+    public List<Route> getRoutes() {
+       return null;
+    }
+
+    public void addRoute(Route r) {
+    }
+}

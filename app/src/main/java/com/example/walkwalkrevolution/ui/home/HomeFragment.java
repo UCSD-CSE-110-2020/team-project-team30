cda19be970aaa2dcaa9ae75774d75df69c1825cc
JiayiA <jiz546@ucsd.edu>
2020-02-06 10:31:05 -0800

    Home Screen Layout

--- app/src/main/java/com/example/walkwalkrevolution/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/walkwalkrevolution/ui/home/HomeFragment.java
@@ -28,7 +28,7 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        final TextView textView = root.findViewById(R.id.text_Progress);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
